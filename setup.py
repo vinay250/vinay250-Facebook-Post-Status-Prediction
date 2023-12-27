3fe6e71254251efb6bd33afa859542aa33bc18a5
“vinayaka <vinayakavirat008@gmail.com>
2023-12-27 18:07:49 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("readme.md","r", encoding="utf-8") as f:
+    long_description = f.read()
+    
+__version__ = "0.0.0"
+
+REPO_NAME = "Facebook Post Status Prediction"
+AUTHOR_USER_NAME = "vinayaka.uppar"
+SCR_REPO = "facebookpostpredict"
+AUTHOR_EMAIL ="vinayakavirat008@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_eamil=AUTHOR_EMAIL,
+    description="A small python package for facebook app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"":"source"},
+    packages=setuptools.find_packages(where="source")
+)
+    
