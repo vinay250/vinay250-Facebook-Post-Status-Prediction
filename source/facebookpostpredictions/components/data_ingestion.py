3fb37d4957c0b9d03827432ce8e1db62e8e401b7
“vinayaka <vinayakavirat008@gmail.com>
2023-12-28 16:27:57 +0530

    commited

--- source/facebookpostpredictions/components/data_ingestion.py
+++ source/facebookpostpredictions/components/data_ingestion.py
@@ -1,12 +1,11 @@
-import pandas as pd  
+import pandas as pd
 from facebookpostpredictions.logger import logging
 from facebookpostpredictions.exception import customexception
 
 import os
 import sys
-from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
 from pathlib import Path
+from sklearn.model_selection import train_test_split
 
 @dataclass
 class DataIngestionConfig:
@@ -26,14 +25,20 @@ class DataIngestion:
             data = pd.read_csv(Path(dataset_path))
             logging.info("Dataset read as a DataFrame")
 
+            # Print debug information
+            logging.info(f"Before creating directories: {os.listdir(Path(self.ingestion_config.raw_data_path).parent)}")
+
             # Save raw dataset
-            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+            os.makedirs(Path(self.ingestion_config.raw_data_path).parent, exist_ok=True)
             data.to_csv(self.ingestion_config.raw_data_path, index=False)
             logging.info("Raw dataset saved in the artifacts folder")
 
+            # Print debug information
+            logging.info(f"After creating directories: {os.listdir(Path(self.ingestion_config.raw_data_path).parent)}")
+
             # Train-test split
             logging.info("Performing train-test split")
-            train_data, test_data = train_test_split(data, test_size=0.25)
+            train_data, test_data = train_test_split(data, test_size=0.25, random_state=42)
             logging.info("Train-test split completed")
 
             # Save train and test datasets
@@ -50,10 +55,10 @@ class DataIngestion:
 
 # Example usage:
 config = DataIngestionConfig(
-    raw_data_path=os.path.join("artifacts", "raw.csv"),
-    train_data_path=os.path.join("artifacts", "train.csv"),
-    test_data_path=os.path.join("artifacts", "test.csv")
+    raw_data_path=os.path.join("artifacts", "raw_data.csv"),
+    train_data_path=os.path.join("artifacts", "train_data.csv"),
+    test_data_path=os.path.join("artifacts", "test_data.csv")
 )
 
 data_ingestion = DataIngestion(config)
-train_data_path, test_data_path = data_ingestion.initiate_data_ingestion(dataset_path="notebooks/data/dataset1.csv")
+train_data_path, test_data_path = data_ingestion.initiate_data_ingestion(dataset_path="E:/Facebook Post Status Prediction/notebooks/data/dataset1.csv")
--- source/facebookpostpredictions/components/data_transformation.py
+++ source/facebookpostpredictions/components/data_transformation.py
@@ -1,5 +1,3 @@
-import os
-import sys
 import pandas as pd
 import numpy as np
 
@@ -7,104 +5,96 @@ from dataclasses import dataclass
 from facebookpostpredictions.exception import customexception
 from facebookpostpredictions.logger import logging
 
+from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OrdinalEncoder,StandardScaler
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.feature_extraction.text import TfidfVectorizer  # Assuming you might want to try TF-IDF
 
 from facebookpostpredictions.utils.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
-
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
 
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config=DataTransformationConfig()
+        self.data_transformation_config = DataTransformationConfig()
 
-        
-    
     def get_data_transformation(self):
-        
         try:
             logging.info('Data Transformation initiated')
-            
+
             # Define which columns should be ordinal-encoded and which should be scaled
-            categorical_cols = ['cut', 'color','clarity']
-            numerical_cols = ['carat', 'depth','table', 'x', 'y', 'z']
-            
+            categorical_cols = ['cut', 'color', 'clarity']
+            numerical_cols = ['carat', 'depth', 'table', 'x', 'y', 'z']
+
             # Define the custom ranking for each ordinal variable
-            cut_categories = ['Fair', 'Good', 'Very Good','Premium','Ideal']
+            cut_categories = ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal']
             color_categories = ['D', 'E', 'F', 'G', 'H', 'I', 'J']
-            clarity_categories = ['I1','SI2','SI1','VS2','VS1','VVS2','VVS1','IF']
-            
+            clarity_categories = ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF']
+
             logging.info('Pipeline Initiated')
-            
-            ## Numerical Pipeline
-            num_pipeline=Pipeline(
+
+            # Numerical Pipeline
+            num_pipeline = Pipeline(
                 steps=[
-                ('imputer',SimpleImputer(strategy='median')),
-                ('scaler',StandardScaler())
-
+                    ('imputer', SimpleImputer(strategy='median')),
+                    ('scaler', StandardScaler())
                 ]
-
             )
-            
-            # Categorigal Pipeline
-            cat_pipeline=Pipeline(
+
+            # Categorical Pipeline
+            cat_pipeline = Pipeline(
                 steps=[
-                ('imputer',SimpleImputer(strategy='most_frequent')),
-                ('ordinalencoder',OrdinalEncoder(categories=[cut_categories,color_categories,clarity_categories])),
-                ('scaler',StandardScaler())
+                    ('imputer', SimpleImputer(strategy='most_frequent')),
+                    ('ordinalencoder', OrdinalEncoder(categories=[cut_categories, color_categories, clarity_categories])),
+                    ('scaler', StandardScaler())
                 ]
-
             )
-            
-            preprocessor=ColumnTransformer([
-            ('num_pipeline',num_pipeline,numerical_cols),
-            ('cat_pipeline',cat_pipeline,categorical_cols)
+
+            preprocessor = ColumnTransformer([
+                ('num_pipeline', num_pipeline, numerical_cols),
+                ('cat_pipeline', cat_pipeline, categorical_cols)
             ])
-            
+
             return preprocessor
-            
 
-            
-            
-        
         except Exception as e:
-            logging.info("Exception occured in the initiate_datatransformation")
+            logging.info("Exception occurred in the initiate_datatransformation")
+            raise customexception(e, sys)
 
-            raise customexception(e,sys)
-            
-    
-    def initialize_data_transformation(self,train_path,test_path):
+    def initialize_data_transformation(self, train_path, test_path):
         try:
-            train_df=pd.read_csv(train_path)
-            test_df=pd.read_csv(test_path)
-            
-            logging.info("read train and test data complete")
-            logging.info(f'Train Dataframe Head : \n{train_df.head().to_string()}')
-            logging.info(f'Test Dataframe Head : \n{test_df.head().to_string()}')
-            
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Read train and test data complete")
+            logging.info(f'Train Dataframe Head:\n{train_df.head().to_string()}')
+            logging.info(f'Test Dataframe Head:\n{test_df.head().to_string()}')
+
             preprocessing_obj = self.get_data_transformation()
-            
+
             target_column_name = 'price'
-            drop_columns = [target_column_name,'id']
-            
-            input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
-            target_feature_train_df=train_df[target_column_name]
-            
-            
-            input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
-            target_feature_test_df=test_df[target_column_name]
-            
-            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            
-            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
-            
+            drop_columns = [target_column_name, 'id']
+
+            input_feature_train_df = train_df.drop(columns=drop_columns, axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=drop_columns, axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
             logging.info("Applying preprocessing object on training and testing datasets.")
-            
+
             train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
@@ -112,17 +102,21 @@ class DataTransformation:
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
             )
-            
-            logging.info("preprocessing pickle file saved")
-            
-            return (
-                train_arr,
-                test_arr
-            )
-            
-        except Exception as e:
-            logging.info("Exception occured in the initiate_datatransformation")
 
-            raise customexception(e,sys)
-            
-    
+            logging.info("Preprocessing pickle file saved")
+
+            return train_arr, test_arr
+
+        except Exception as e:
+            logging.info("Exception occurred in the initiate_datatransformation")
+            raise customexception(e, sys)
+
+# Example usage:
+# Assuming you have paths for your train and test datasets
+train_data_path = "path/to/your/train.csv"
+test_data_path = "path/to/your/test.csv"
+
+data_transformation = DataTransformation()
+train_arr, test_arr = data_transformation.initialize_data_transformation(train_data_path, test_data_path)
+
+# Now, you can use train_arr and test_arr for your model training
