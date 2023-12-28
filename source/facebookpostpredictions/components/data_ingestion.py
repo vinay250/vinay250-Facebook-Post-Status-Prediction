9dcdc3df8a606a45271e0adb5b06e505b62a9848
“vinayaka <vinayakavirat008@gmail.com>
2023-12-28 16:56:19 +0530

    commted

--- source/facebookpostpredictions/components/data_ingestion.py
+++ source/facebookpostpredictions/components/data_ingestion.py
@@ -1,64 +1,59 @@
-import pandas as pd
-from facebookpostpredictions.logger import logging
-from facebookpostpredictions.exception import customexception
-
 import os
 import sys
-from pathlib import Path
+from facebookpostpredictions.exception import CustomException
+from facebookpostpredictions.logger import logging
+import pandas as pd
+
 from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
 
 @dataclass
 class DataIngestionConfig:
-    raw_data_path: str
-    train_data_path: str
-    test_data_path: str
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
-    def __init__(self, config: DataIngestionConfig):
-        self.ingestion_config = config
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
 
-    def initiate_data_ingestion(self, dataset_path: str):
-        logging.info("Data ingestion started")
-
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
         try:
-            # Read dataset
-            data = pd.read_csv(Path(dataset_path))
-            logging.info("Dataset read as a DataFrame")
+            df=pd.read_csv('notebooks\data\dataset1.csv')
+            logging.info('Read the dataset as dataframe')
 
-            # Print debug information
-            logging.info(f"Before creating directories: {os.listdir(Path(self.ingestion_config.raw_data_path).parent)}")
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
-            # Save raw dataset
-            os.makedirs(Path(self.ingestion_config.raw_data_path).parent, exist_ok=True)
-            data.to_csv(self.ingestion_config.raw_data_path, index=False)
-            logging.info("Raw dataset saved in the artifacts folder")
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
-            # Print debug information
-            logging.info(f"After creating directories: {os.listdir(Path(self.ingestion_config.raw_data_path).parent)}")
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
-            # Train-test split
-            logging.info("Performing train-test split")
-            train_data, test_data = train_test_split(data, test_size=0.25, random_state=42)
-            logging.info("Train-test split completed")
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
-            # Save train and test datasets
-            train_data.to_csv(self.ingestion_config.train_data_path, index=False)
-            test_data.to_csv(self.ingestion_config.test_data_path, index=False)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Data ingestion completed")
+            logging.info("Inmgestion of the data iss completed")
 
-            return self.ingestion_config.train_data_path, self.ingestion_config.test_data_path
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
 
+            )
         except Exception as e:
-            logging.error("Exception occurred during data ingestion")
-            raise customexception(e, sys)
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
-# Example usage:
-config = DataIngestionConfig(
-    raw_data_path=os.path.join("artifacts", "raw_data.csv"),
-    train_data_path=os.path.join("artifacts", "train_data.csv"),
-    test_data_path=os.path.join("artifacts", "test_data.csv")
-)
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-data_ingestion = DataIngestion(config)
-train_data_path, test_data_path = data_ingestion.initiate_data_ingestion(dataset_path="E:/Facebook Post Status Prediction/notebooks/data/dataset1.csv")
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+
+
